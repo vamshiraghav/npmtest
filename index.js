77cdcca103007207e196491228fe47467f9577b0
Vamshiraghav <vamshiraghav.yeligati@netrovert.com>
2021-05-22 19:24:23 +0530

    some changes

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 exports.printMsg = function() {
     console.log("This is a message from the demo 3d learn package \n new data added ");
   }
+
+  exports.gitMsg=()=>{
+      console.log("this is new modified function with the github package integration")
+  }
