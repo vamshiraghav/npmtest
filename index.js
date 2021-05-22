4b14cbe1a56a275881ff0588c62c45f0fb5aef1a
Vamshiraghav <vamshiraghav.yeligati@netrovert.com>
2021-05-22 17:40:56 +0530

    new data

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 exports.printMsg = function() {
-    console.log("This is a message from the demo 3d learn package");
+    console.log("This is a message from the demo 3d learn package \n new data added ");
   }
